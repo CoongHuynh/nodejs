80b9ee579c4e4905441879927e71fada80382eeb
CoongHuynh <huynhcong.dev@gmail.com>
2024-07-03 17:31:28 +0700

    75 put method

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const routerAPI = express.Router();
-const { getUsersAPI } = require("../controllers/apiController");
+const {
+  getUsersAPI,
+  postCreateUserAPI,
+} = require("../controllers/apiController");
 
 routerAPI.get("/", (req, res) => {
   res.send("Hello world with apis");
 });
 
-routerAPI.get("/abc", (req, res) => {
-  res.status(200).json({
-    data: "hello world with apis",
-  });
-});
-
 routerAPI.get("/users", getUsersAPI);
+
+routerAPI.post("/users", postCreateUserAPI);
 
 module.exports = routerAPI;
